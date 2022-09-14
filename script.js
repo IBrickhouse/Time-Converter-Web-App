a0f4589bca68682d2707d76c5924d32300b0d26e
IBrickhouse <mrfurbyb@gmail.com>
2022-09-14 15:51:26 -0700

    Upload Previously Done Work

---
+++ script.js
@@ -0,0 +1,37 @@
+// var convertedDate = "2000-09-21";
+// var convertedTime = "12:00"
+
+// function changeTimezone() {
+
+//     let date = 
+//         new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+//     console.log('Given IST datetime: ' + date);
+  
+//     let usaTime = 
+//         date.toLocaleString("en-US", {
+//             timeZone: "America/New_York" 
+//         });
+//     //console.log('USA datetime: ' + usaTime);
+// }
+
+function processForm() {
+    var date = document.getElementById("when").value;
+    var time = document.getElementById("whenTime").value;
+    var timezone = document.getElementById("whenZone")
+
+    return date
+}
+
+console.log(date)
+
+function convert(){
+
+}
+
+// Puts date on screen
+document.getElementById("convertedDate").value = convertedDate;
+
+document.getElementById("convertedTime").value = convertedTime;
+
+
+

19131c28abf8872b95d3e44d0e29de03cba47b73
IBrickhouse <mrfurbyb@gmail.com>
2022-11-10 18:28:22 -0800

    Basic Read Data From Database Implemented
    Access names of companies, groups, and members along with a handful of important dates.

--- eventcal.js
+++ eventcal.js
@@ -7,8 +7,8 @@ const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 async function loadData() {
     const { data, error } = await _supabase
-            .from('groups')
-            .select()
+            .from('members')
+            .select('*')
 
     if(!error) {
         //loop display data here
@@ -16,7 +16,8 @@ async function loadData() {
 
         let contents = ''
         data.forEach(function(item){
-            contents += `<div> ${item.name} - ${item.debut}</div>` 
+            contents += `<div> ${item.romanized_name} - ${item.birthday}</div>`
+ 
         })
 
         parent.insertAdjacentHTML('beforeend', contents)
@@ -24,5 +25,96 @@ async function loadData() {
     
 }
 
-// I believe this should be run at each load. Supabase's own code
-loadData()
+async function loadCompanyGroups() {
+    const { data, error } = await _supabase
+        .from('companies')
+        .select(`*, groups(*)`)
+        // .eq('company_id', 'eb6903f5-bcd5-419a-9fdc-73ed92f811c1')
+
+    if(!error) {
+    //loop display data here
+    const parent = document.getElementById('holder')
+
+        let contents = ''
+        data.forEach(function(item){
+            console.log(item);
+            // contents += `<div> ${item.romanized_name} - ${item.birthday}</div>` 
+        })
+
+        parent.insertAdjacentHTML('beforeend', contents)
+    }
+}
+
+async function loadGroupMembers() {
+    const { data, error } = await _supabase
+        .from('groups')
+        .select(`name, members(romanized_name)`)
+        // .eq('company_id', 'eb6903f5-bcd5-419a-9fdc-73ed92f811c1')
+
+    if(!error) {
+    //loop display data here
+    const parent = document.getElementById('holder')
+
+        let contents = ''
+        data.forEach(function(item){
+            console.log(item);
+            contents += `<div> ${item.name} - ${item.members}</div>`
+            
+        })
+
+        parent.insertAdjacentHTML('beforeend', contents)
+    }
+}
+
+async function groupMemberNamesOnScreen() {
+    const { data, error } = await _supabase
+        .from('groups')
+        .select(`name, members(*)`)
+        // .eq('company_id', 'eb6903f5-bcd5-419a-9fdc-73ed92f811c1')
+
+    if(!error) {
+    //loop display data here
+        const parent = document.getElementById('holder')
+
+        let contents = ''
+        data.forEach(function(group){
+
+            contents += `<div> ${group.name}</div>`
+
+            group.members.forEach(function(member) {
+                contents += `<div> &emsp; ${member.romanized_name} - ${member.birthday} </div>`
+            })
+        })
+
+        parent.insertAdjacentHTML('beforeend', contents)
+    }
+}
+
+async function companiesGroupNamesOnScreen() {
+    const { data, error } = await _supabase
+        .from('companies')
+        .select(`name, groups(*)`)
+        // .eq('company_id', 'eb6903f5-bcd5-419a-9fdc-73ed92f811c1')
+
+    if(!error) {
+    //loop display data here
+        const parent = document.getElementById('holder')
+
+        let contents = ''
+        data.forEach(function(company){
+
+            contents += `<div> ${company.name}</div>`
+
+            company.groups.forEach(function(group) {
+                contents += `<div> &emsp; ${group.name} -${group.debut} </div>`
+            })
+        })
+
+        parent.insertAdjacentHTML('beforeend', contents)
+    }
+}
+
+// companiesGroupNamesOnScreen();
+groupMemberNamesOnScreen();
+// loadCompanyGroups();
+// loadGroupMembers();

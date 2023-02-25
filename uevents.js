7b715fca82397539761c3487fb4f7b2325da1767
IBrickhouse <mrfurbyb@gmail.com>
2023-02-25 16:59:39 -0700

    Create function to insert a row into another table
    References: 22

--- uevents.js
+++ uevents.js
@@ -23,4 +23,83 @@ async function loadData() {
     
 }
 
-loadData();
+async function findPastEventRecord() {
+
+    var currentTime = new Date()
+    var month = currentTime.getMonth() + 1
+    var day = currentTime.getDate()
+    var year = currentTime.getFullYear()
+    var today =(String(year) + "-" + String(month) .padStart(2,'0')) + "-" + String(day) .padStart(2,'0');
+    console.log(today);
+    
+    const { data, error } = await _supabase
+            .from('upcomingevents')
+            .select('*')
+            .eq('date', today)
+            
+    if(!error) {
+
+        //console.log(data);
+
+        data.forEach(function(item){
+            console.log(item);
+            insertRow(item);
+        })
+
+        //loop display data here
+        const parent = document.getElementById('holder')
+
+        let contents = ''
+        data.forEach(function(item){
+            contents += `<div> &#8226; ${item.name} - ${item.date} - ${item.time} - ${item.timezone} - ${item.description} </div>`
+
+        })
+
+        parent.insertAdjacentHTML('beforeend', contents)
+
+    }
+    
+}
+
+async function insertRow(pastRow) {
+    const { data, error } = await _supabase
+    .from('upcomingevents_test')
+    .insert([
+      {name: pastRow.name, description: pastRow.description, date: pastRow.date, time: pastRow.time, timezone: pastRow.timezone, recurring: pastRow.recurring},
+    ])
+    
+}
+
+async function removeRow() {
+    const { data, error } = await _supabase
+            .from('upcomingevents_test')
+            .delete('data')
+            .eq('date', today)
+            
+
+    if(!error) {
+
+        const { data, error } = await supabase
+        .from('upcomingevents_test')
+        .insert([
+            { name: 'name', description: 'description', date: 'date', time: 'time'},
+        ])
+
+        // console.log(data);
+        // //loop display data here
+        // const parent = document.getElementById('holder')
+
+        // let contents = ''
+        // data.forEach(function(item){
+        //     contents += `<div> &#8226; ${item.name} - ${item.date} - ${item.time} - ${item.timezone} - ${item.description} </div>`
+ 
+        // })
+
+        // parent.insertAdjacentHTML('beforeend', contents)
+    }
+    
+}
+
+// loadData();
+
+findPastEventRecord();
